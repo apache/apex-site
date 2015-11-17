d0abe5e13c0c94c05042ccbf9e2c0f4140c40342
Andy Perlitch <andy@datatorrent.com>
2015-11-17 11:24:11 -0800

    from 3613f95acedc20b09c6027a070f4f0b155057ce3

---
+++ content/js/docs/apache-docs.js
@@ -0,0 +1,13 @@
+/* global angular:false */
+
+'use strict';
+
+angular.module('apache-docs', [])
+  
+  .directive('docsSideBar', function() {
+    return {
+      link: function(scope, element) {
+        
+      }
+    };
+  });
