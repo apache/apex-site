439b0d9838083802b49bdf07e334726cd0102f9c
Thomas Weise <thw@apache.org>
2016-12-08 08:53:04 -0800

    Update download page for Malhar 3.6 release.

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ rm -rf content/docs
 cp -R docs content/docs
 git add content
 echo "Commiting changes to asf-site branch from master branch."
-git commit -m "from $COMMIT_HASH"
+git commit --author "Apex Dev <dev@apex.apache.org>" -m "from $COMMIT_HASH"
 echo "-----------------------------------------------------"
 echo "BUILD SUCCESSFUL. You are now on the asf-site branch."
 echo "Run git push origin asf-site to update the live site."
